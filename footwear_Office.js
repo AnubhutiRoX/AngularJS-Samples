b6dc238ba4118275f2bd0f12f8dc78c08030190a
vinnytcs <s.anubhuti1987@gmail.com>
2016-06-30 08:58:02 +0530

    Create footwear_Office.js

---
+++ footwear_Office.js
@@ -0,0 +1,6 @@
+myApp.controller("footwearSectionCtrl",function($scope){
+
+	$scope.footwearTypes = [{imgSrc1:'Pictures/Footwear1.jpg',category1:'casual',imgSrc2:'Pictures/Footwear2.jpg',category2:'Sneakers',imgSrc3:'Pictures/Footwear2.jpg',category3:'Sneakers'},{imgSrc1:'Pictures/Footwear1.jpg',category1:'Snowshoe',imgSrc2:'Pictures/Footwear2.jpg',category2:'Peep-toe shoe',imgSrc3:'Pictures/Footwear2.jpg',category3:'Sneakers'},{imgSrc1:'Pictures/Footwear1.jpg',category1:'Boot',imgSrc2:'Pictures/Footwear2.jpg',category2:'Steel-toe boot',imgSrc3:'Pictures/Footwear2.jpg',category3:'Sneakers'},{imgSrc1:'Pictures/Footwear1.jpg',category1:'Shoe',imgSrc2:'Pictures/Footwear2.jpg',category2:'Skate shoe',imgSrc3:'Pictures/Footwear2.jpg',category3:'Sneakers'},{imgSrc1:'Pictures/Footwear1.jpg',category1:'Bovver boot',imgSrc2:'Pictures/Footwear2.jpg',category2:'Flipsters',imgSrc3:'Pictures/Footwear2.jpg',category3:'Sneakers'},{imgSrc1:'Pictures/Footwear1.jpg',category1:'Slipper',imgSrc2:'Pictures/Footwear2.jpg',category2:'Shank',imgSrc3:'Pictures/Footwear2.jpg',category3:'Sneakers'}];
+	
+	$('#myCarousel').hide();
+});
